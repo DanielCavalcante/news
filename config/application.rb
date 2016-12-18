1b94b14bd5bb649d926f1a3899b5b67097bb42e4
Daniel Cavalcante <danielcdesouza@gmail.com>
2016-12-18 16:05:30 -0300

    Up project

---
+++ config/application.rb
@@ -0,0 +1,26 @@
+require_relative 'boot'
+
+require "rails"
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "action_cable/engine"
+# require "sprockets/railtie"
+require "rails/test_unit/railtie"
+
+Bundler.require(*Rails.groups)
+
+module News
+  class Application < Rails::Application
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options, :delete, :put]
+      end
+    end
+    config.api_only = true
+  end
+end
